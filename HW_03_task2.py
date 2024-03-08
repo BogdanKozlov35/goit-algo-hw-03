7ae803f81cf6bac2ee7c0acd51f75f1dd6a4958b
BogdanKozlov35 <bkozlov35@gmail.com>
2024-03-09 01:45:55 +0200

    Initial commit

--- HW_03_task2.py
+++ HW_03_task2.py
@@ -1,29 +1,14 @@
 import random
-def get_numbers_ticket(min, max, quantity):
-    # ввід та перевірка введених чисел
+def get_numbers_ticket(min: int, max: int, quantity: int):
+    # перевірка чисел
     all_num = []
-    if type(min) == int and type(max) == int and type(quantity) == int:
-
-        if quantity >= 1 and quantity <= 6:
-
-            if max >= (1 + quantity) and max <=1000:
-
-                if min >= 1  and min < (max - quantity):
-                    for num in range (min, max):
-                        all_num.append(num)
-                        #print(all_num)
-                    lottery_numbers = random.sample(all_num, quantity)
-                    lottery_numbers.sort()
-
-                    print(f"Ваші лотерейні числа: {lottery_numbers}")
-                else:
-                    print("min number out of range")
-            else:
-                print("max number out of range")
-        else:
-            print("quantity out of range")
-
+    if quantity <= max and quantity >= min and max <= 1000 and min >=1:
+         for num in range (min, max):
+             all_num.append(num)
+         lottery_numbers = random.sample(all_num, quantity)
+         lottery_numbers.sort()
+         return lottery_numbers
     else:
-        return print("use integer numbers")
-
-get_numbers_ticket(1, 60, 5)
+        return None
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
