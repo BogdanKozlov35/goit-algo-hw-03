2322107014d1afa4accb076ed5f8d043556984fa
BogdanKozlov35 <bkozlov35@gmail.com>
2024-03-09 00:19:55 +0200

    Initial commit

--- HW_03_task1.py
+++ HW_03_task1.py
@@ -1,20 +1,17 @@
 from datetime import datetime
-def get_days_from_today():
+def get_days_from_today(date):
+    current_date = datetime.now()
+    current_date = current_date.date()
     try:
-        date = input("Enter the date in the format YYYY-MM-DD: ")
         target_day = datetime.strptime(date, "%Y-%d-%m")
         target_day = target_day.date()
 
-        current_date = datetime.now()
-        current_date = current_date.date()
-
         delta_date = target_day - current_date
 
-        print(f"The {delta_date.days} days left after today")
         return delta_date.days
-        print(f"The {delta_date.days} day(s) left after today")
-    except ValueError:
-        print("*** the date isn't correct ***")
-        return get_days_from_today()
 
-get_days_from_today()
+    except ValueError or TypeError:
+        print("*** the date isn't correct ***")
+        return None
+
+print(get_days_from_today("2021-10-09"))
