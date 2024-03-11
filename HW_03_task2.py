e86c98c8dec49009393a64b048ff660a2c28eeaf
BogdanKozlov35 <bkozlov35@gmail.com>
2024-03-11 16:17:14 +0200

    Initial commit

--- HW_03_task2.py
+++ HW_03_task2.py
@@ -1,14 +1,17 @@
 import random
 def get_numbers_ticket(min: int, max: int, quantity: int):
-    # перевірка чисел
+
     all_num = []
-    if quantity <= max and quantity >= min and max <= 1000 and min >=1:
-        for num in range (min, max):
-            all_num.append(num)
-        lottery_numbers = random.sample(all_num, quantity)
-        lottery_numbers.sort()
-        return lottery_numbers
+    if max <= 1000:
+        if min >= 1:
+            if 1 <= quantity <= (max - min + 1):
+                for num in range(min, (max +1)):
+                    all_num.append(num)
+                lottery_numbers = random.sample(all_num, quantity)
+                lottery_numbers.sort()
+                return lottery_numbers
     else:
         return all_num
-lottery_numbers = get_numbers_ticket(1, 55, 6)
+
+lottery_numbers = get_numbers_ticket(10, 20, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
